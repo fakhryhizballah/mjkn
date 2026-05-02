07f3f0735249c25de3add785633850ee4db29516
fakhryhizballah <falehry88@gmail.com>
2026-05-02 21:47:19 +0700

    index

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
         name: "node-mjkn",
-        script: "./app.js",
+        script: "./index.js",
         watch: true,
         env: {
             NODE_ENV: "development",
