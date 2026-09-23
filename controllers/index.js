7177cf13fe235d01360c8b0ff7aa1ae9d73febc9
fakhryhizballah <falehry88@gmail.com>
2026-09-23 16:14:09 +0700

    fix: adjust estimasiWaktu calculation for timezone offset
    Adjust estimasiWaktu to account for the 7‑hour UTC offset when creating

--- controllers/index.js
+++ controllers/index.js
@@ -645,7 +645,7 @@ const ambilAntrean = async (req, res) => {
         const transactionResult = await sequelize.transaction(async (t) => {
             
             // Insert Referensi Mobile JKN
-            const estimasiWaktu = new Date(`${decode.tanggalperiksa} ${dataJadwal.jam_mulai}`).getTime() + (5 * 60 * 1000);
+            const estimasiWaktu = new Date(`${decode.tanggalperiksa} ${dataJadwal.jam_mulai}`).getTime() + (5 * 60 * 1000) - (7 * 60 * 60 * 1000);
             const kuotaSisaUpdate = dataJadwal.kuota - sisakuota - 1;
 
             await referensi_mobilejkn_bpjs.create({
